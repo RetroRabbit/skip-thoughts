2c14e11665e107ba011b19e5ced39b94a46a984d
jaderabbit <jabbott@retrorabbit.co.za>
2016-10-12 12:45:30 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Skip Thoughts',
+      version='1.0',
+      description='Sent2Vec encoder and training code from the paper Skip-Thought Vectors.',
+      author='Ryan Kiros',
+      url='https://github.com/ryankiros/skip-thoughts',
+      packages=['skipthoughts', 'skipthoughts.training','skipthoughts.decoding'],
+     )
