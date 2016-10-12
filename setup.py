de09076720dab36d7b3b320dfd6c02dbd3a166f9
jaderabbit <jabbott@retrorabbit.co.za>
2016-10-12 13:16:34 +0200

    Fixes to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
+import os
+from setuptools import setup, find_packages
 
-from distutils.core import setup
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='Skip Thoughts',
       version='1.0',
       description='Sent2Vec encoder and training code from the paper Skip-Thought Vectors.',
       author='Ryan Kiros',
       url='https://github.com/ryankiros/skip-thoughts',
-      packages=['skipthoughts', 'skipthoughts.training','skipthoughts.decoding'],
+      long_description=read('README.md'),
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
       install_requires=['theano','keras','nltk','scikit-learn','gensim','scipy']
      )
